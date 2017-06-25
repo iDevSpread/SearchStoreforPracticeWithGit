736eda7e5480f94823c60040fe4d491ba17af773
ShareValue <wingo0324@gmail.com>
2017-06-25 18:17:03 -0230

    Practice of App

---
+++ SearchStore/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchStore
+//
+//  Created by Kai Wang on 2017-06-25.
+//  Copyright © 2017 Kai Wang. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
