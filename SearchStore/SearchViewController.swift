eff38d422b80f5aa537546ca8f93c8f93d70f7bd
ShareValue <wingo0324@gmail.com>
2017-06-27 17:16:37 -0230

    add required methods for UITableView

--- SearchStore/SearchViewController.swift
+++ SearchStore/SearchViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+	@IBOutlet weak var searchBar: UISearchBar!
+	@IBOutlet weak var tableView: UITableView!
+	
+	var searchResults: [String] = []
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		tableView.contentInset = UIEdgeInsetsMake(64, 0, 0, 0)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -23,3 +31,37 @@ class SearchViewController: UIViewController {
 
 }
 
+// send search result to show
+extension SearchViewController: UISearchBarDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return searchResults.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cellIdentifier = "SearchResultCell"
+		var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+		if cell == nil {
+			cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+		}
+		
+		cell.textLabel!.text = searchResults[indexPath.row]
+		return cell
+	}
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		searchBar.resignFirstResponder() // it can be used in the UITextView
+		searchResults = [] // Perform every result into the variable instances
+		for i in 0...2{
+			searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+		}
+		tableView.reloadData()
+	}
+}
+
+extension SearchViewController: UITableViewDataSource {
+	
+}
+
+extension SearchViewController: UITableViewDelegate {
+	
+}
