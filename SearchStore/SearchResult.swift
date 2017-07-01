47ec8d261e73b974c5638fde6aa10467870941b4
ShareValue <wingo0324@gmail.com>
2017-07-01 11:05:26 -0230

    add more properties of a search result

--- SearchStore/SearchResult.swift
+++ SearchStore/SearchResult.swift
@@ -9,5 +9,11 @@
 class SearchResult {
 	var name = ""
 	var artistName = ""
-	
+	var artworkSmallURL = ""
+	var artworkLargeURL = ""
+	var storeURL = ""
+	var kind = ""
+	var currency = ""
+	var price = 0.0
+	var genre = ""
 }
